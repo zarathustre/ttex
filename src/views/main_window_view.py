3d0a5d35129b5e5823bea849358d7b964081ee70
zarathustre <salim_salloum@hotmail.com>
2021-09-24 18:58:33 +0200

    Changed database implementation
    Database is now a class, so an object needs to be created.
    Also added commenting

--- src/views/main_window_view.py
+++ src/views/main_window_view.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QMainWindow, QMessageBox
 from src.uic.main_window import Ui_MainWindow
 from src.views.create_scenario_view import CreateScenario
-import lite
+from lite import Database
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -16,109 +16,89 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.create_scenario_button.clicked.connect(lambda: self.create_scenario())
 
 
+    # Handles the creation of a new scenario
     def create_scenario(self):
-        self.scenario = CreateScenario()
-        self.main_stack.addWidget(self.scenario)
-        self.main_stack.setCurrentIndex(1)
-        self.scenario.back_button.clicked.connect(lambda: clear_and_back())
-        self.scenario.create_scenario_tab.currentChanged.connect(lambda: on_tab_change())
+        #region
+        self.scenario = CreateScenario()              # create scenario object
+        self.main_stack.addWidget(self.scenario)      # add object to main stack
+        self.main_stack.setCurrentIndex(1)            # switch main stack to show created object
+
+        # assign widgets
+        self.scenario.back_button.clicked.connect(lambda: clear_and_back())                     # back button
+        self.scenario.create_scenario_tab.currentChanged.connect(lambda: on_tab_change())       # create scenario tabbed widget
 
 
+        # Handles the tab change in the create scenario page
+        # On last tab, change next button to save button
         def on_tab_change():
             tab = self.scenario.create_scenario_tab
             next = self.scenario.next_button
-            if tab.currentWidget().objectName() == 'tab_3':
+            if tab.currentWidget().objectName() == 'tab_3':         # last tab
                 next.setText("Save")
                 next.clicked.disconnect()
-                next.clicked.connect(lambda: save_and_back())
+                next.clicked.connect(lambda: save_and_back())       # next button becomes save button to save entries in database
             else:
                 next.setText("Next")
                 next.clicked.disconnect()
-                next.clicked.connect(lambda: tab.setCurrentIndex(tab.currentIndex() + 1))
+                next.clicked.connect(lambda: tab.setCurrentIndex(tab.currentIndex() + 1))   # next button changes tab to the next one
 
 
-        def save_and_back():
-            #region
-            data = self.scenario.get_values()
-            empty = False
-            for item in data[2]:
-                if item == "":
-                    empty = True
-            for item in data[3]:
-                if item == "":
-                    empty = True
-            for item in data[4]:
-                if item == "":
-                    empty = True
-            for item in data[5]:
-                if item == "":
-                    empty = True
-
-            if data[0] == "" or data[1] == "" or empty:
-                QMessageBox.warning(self.scenario, "Warning", "All fields must be filled !", QMessageBox.Ok)
-            else:
-                save_to_db(data)
-            #endregion
-                
-
+        # Change the main stack to the first one and delete the create scenario object
         def clear_and_back():
             self.main_stack.setCurrentIndex(0) 
-            self.scenario.deleteLater()
+            self.scenario.deleteLater()         # delete create scenario object
 
 
-        def save_to_db(data):
-            #region
-            cnx = lite.create_connection('scenes.db')
-            data[0] = data[0].lower()
+        # Save entries of the create scenario in the database
+        def save_and_back():
+            db = Database('scenes.db')              # create Database object
+            db.create_db()                          # create tables if they do not exist
 
-            # Create scenario table
-            create_scenarios_table = """ 
-            CREATE TABLE IF NOT EXISTS scenarios 
-            (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT UNIQUE, scenario TEXT,
-            o1 TEXT, o2 TEXT, o3 TEXT, o4 TEXT, o5 TEXT,
-            i1 TEXT, i2 TEXT, i3 TEXT, i4 TEXT, i5 TEXT)
-            """
-            lite.execute_query(cnx, create_scenarios_table)
+            data = self.scenario.get_values()       # get values from text fields
+            data[0] = data[0].lower()               # lower case title to check for uniqueness
 
-            # Create questions / answers / weights table
-            create_qaw_table = """
-            CREATE TABLE IF NOT EXISTS qaw (scenario INTEGER, question TEXT, answer TEXT, weight INTEGER,
-            FOREIGN KEY (scenario) REFERENCES scenarios(id))
-            """
-            lite.execute_query(cnx, create_qaw_table)
+            get_title = "SELECT title FROM scenarios WHERE title = ?"
+            title = db.query_db(get_title, [data[0]]) 
 
-            # Check if title exists
-            get_title = """ SELECT title FROM scenarios WHERE title = ? """
-            title = lite.fetch_query_var(cnx, get_title, [data[0]])
-            
-            if title:
-                QMessageBox.warning(self.scenario, "Warning", "Title already easists !", QMessageBox.Ok)
+            empty = False           # this variable is true if any text field is empty
+            for i in range(2,6):
+                for item in data[i]:
+                    if item == "":
+                        empty = True        
+
+            # if there are empty fields
+            if data[0] == "" or data[1] == "" or empty:
+                QMessageBox.warning(self.scenario, "Warning", "All fields must be filled !", QMessageBox.Ok)
+            # if question and answer fields are not equal
             elif len(data[4]) != len(data[5]):
                 QMessageBox.warning(self.scenario, "Warning", "All questions must be associated with an answer !", QMessageBox.Ok)
+            # if title already exists in the database
+            elif title:
+                QMessageBox.warning(self.scenario, "Warning", "Title already exists !", QMessageBox.Ok)
+            # constraints done -> save in database
             else:
                 # Insert title / scenario
-                insert_scenario = """ INSERT INTO scenarios (title, scenario) VALUES (?, ?) """
-                lite.execute_query_var(cnx, insert_scenario, [data[0], data[1]])
+                insert_scenario = "INSERT INTO scenarios (title, scenario) VALUES (?, ?)"
+                db.query_db(insert_scenario, [data[0], data[1]])
 
                 # Get scenario id
-                get_scenario_id = """ SELECT id FROM scenarios WHERE title = ? """
-                id = lite.fetch_query_var(cnx, get_scenario_id, [data[0]])[0][0]
+                get_id = "SELECT id FROM scenarios WHERE title = ?"
+                id = db.query_db(get_id, [data[0]])[0][0]
 
                 # Insert objectives
-                for i in range(0, len(data[2])):
-                    q = " UPDATE scenarios SET o" + str(i+1) + " = ? WHERE id = ? "
-                    lite.execute_query_var(cnx, q, [data[2][i], id])
+                for i in range(len(data[2])):
+                    q = "UPDATE scenarios SET o" + str(i+1) + " = ? WHERE id = ?"
+                    db.query_db(q, [data[2][i], id])
 
                 # Insert injects
-                for i in range(0, len(data[3])):
-                    q = " UPDATE scenarios SET i" + str(i+1) + " = ? WHERE id = ? "
-                    lite.execute_query_var(cnx, q, [data[3][i], id])
+                for i in range(len(data[3])):
+                    q = "UPDATE scenarios SET i" + str(i+1) + " = ? WHERE id = ?"
+                    db.query_db(q, [data[3][i], id])
 
                 # Insert questions / answers / weights
                 for i in range(len(data[4])):
-                    q = """ INSERT INTO qaw VALUES (?, ?, ?, ?) """
-                    lite.execute_query_var(cnx, q, [id, data[4][i], data[5][i], data[6][i]])
-                
-                cnx.close()
-                clear_and_back()
-            #endregion    
+                    q = "INSERT INTO qaw VALUES (?, ?, ?, ?)"
+                    db.query_db(q, [id, data[4][i], data[5][i], data[6][i]])
+
+                clear_and_back()    # change main stack to first page and delete create scenario object
+        #endregion
